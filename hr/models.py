10cc881fc7b077397b380afb0eab0380129c3c2c
Victor Ryabov <victorwillhard@gmail.com>
2024-11-01 12:17:29 +0300

    feat: update Employee model and register to admin-panel

--- hr/models.py
+++ hr/models.py
@@ -1,6 +1,25 @@
+import uuid
 from django.db import models
 
 
 class Employee(models.Model):
+    class Workplace(models.IntegerChoices):
+        JOB_1 = 1, "Junior"
+        JOB_2 = 2, "Middle"
+        JOB_3 = 3, "Senior"
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    workspace = models.PositiveSmallIntegerField(
+        choices=Workplace.choices,
+        default=Workplace.JOB_1,
+        help_text="Choose your workplace",
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name
